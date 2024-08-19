a692e27ff8ef9e7d4cc491723efb96ad73f5786b
Sergio <danichidominecraft@gmail.com>
2024-08-19 14:37:10 -0600

    Dummy Variables

--- gym_full_app/src/environments/environments.ts
+++ gym_full_app/src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
 
-    cognitoUserPoolId: '',
-    cognitoAppClientId: '',
+    cognitoUserPoolId: 'YourCognitoUserPoolId',
+    cognitoAppClientId: 'YourCognitoAppClientId',
 
 };
