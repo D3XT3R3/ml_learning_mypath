0b6c719eec4b4a9752ee5964404573693c8b726d
Sergio <danichidominecraft@gmail.com>
2024-08-21 15:43:09 -0600

    LocalStorage Cognito implementation in Angular

--- gym_full_app/src/environments/environments.ts
+++ gym_full_app/src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-
+    region: 'your-region',
     cognitoUserPoolId: 'YourCognitoUserPoolId',
     cognitoAppClientId: 'YourCognitoAppClientId',
 
--- gym_full_app/src/login.cognito/infra.login/cognito.client.ts
+++ gym_full_app/src/login.cognito/infra.login/cognito.client.ts
@@ -1,15 +1,75 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, from } from "rxjs";
+import { map } from "rxjs/operators";
 import {
     CognitoIdentityProviderClient, 
     InitiateAuthCommand,
-    SignUpCommand,
     GlobalSignOutCommand,
     InitiateAuthCommandOutput,
-    SignUpCommandOutput,
-    GlobalSignOutCommandOutput
+    GetUserCommand,
+    GlobalSignOutCommandOutput,
     } from "@aws-sdk/client-cognito-identity-provider";
     import { environment } from "../../environments/environments";
 
 
-    //TOOD: Add logic for Login logic for Cognito
+@Injectable({
+    providedIn: 'root'
+  })
+  export class CognitoClient {
+    private client: CognitoIdentityProviderClient;
+  
+    constructor() {
+      this.client = new CognitoIdentityProviderClient({ 
+        region: environment.region 
+      });
+    }
+  
+    signIn(username: string, password: string): Observable<InitiateAuthCommandOutput> {
+      const command = new InitiateAuthCommand({
+        AuthFlow: "USER_PASSWORD_AUTH",
+        ClientId: environment.cognitoAppClientId,
+        AuthParameters: {
+          USERNAME: username,
+          PASSWORD: password
+        }
+      });
+  
+      return from(this.client.send(command));
+    }
+  
+   /* signUp(username: string, password: string, email: string): Observable<boolean> {
+      const command = new SignUpCommand({
+        ClientId: environment.cognitoAppClientId,
+        Username: username,
+        Password: password,
+        UserAttributes: [
+          {
+            Name: "email",
+            Value: email
+          }
+        ]
+      });
+  
+      return from(this.client.send(command)).pipe(
+        map((result: SignUpCommandOutput) => !!result.UserSub)
+      );
+    }
+  */
+
+    signOut(accessToken: string): Observable<GlobalSignOutCommandOutput> {
+      const command = new GlobalSignOutCommand({
+        AccessToken: accessToken
+      });
+      return from(this.client.send(command));
+    }
+
+    getCurrentUser(accessToken: string): Observable<any> {
+        const command = new GetUserCommand({
+            AccessToken: accessToken
+        });
+        return from(this.client.send(command));
+   }
+}
+
+
+  
