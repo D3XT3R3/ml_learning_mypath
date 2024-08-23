a082b0acdc9aeb2a748dc7a47099582e9d3a1e78
Sergio <danichidominecraft@gmail.com>
2024-08-23 13:48:27 -0600

    ETL Part 1.

---
+++ ETL/experimentation/basics/dynamo_to_dynamo/config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# AWS regions
+
+SOURCE_REGION = 'Your Source Region'
+DESTINATION_REGION = 'Your Destination Region'
+
+# DynamoDB tables
+SOURCE_TABLE_NAME = 'Your Source Table Name'
+DESTINATION_TABLE_NAME = 'Your Destination Table Name'
+
+#S3 Configuration
+S3_BUCKET_NAME = 'Your S3 Bucket Name'
+S3_KEY_PREFIX = 'Your S3 Key Prefix'
+
+# Batch size
+BATCH_SIZE = 0
+
